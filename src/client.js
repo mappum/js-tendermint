47c353a5198962d0c15477bee5c5948adb8b6f26
Matt Bell <mappum@gmail.com>
2017-01-16 19:17:51 -0800

    Removed dependencies that result in large bundles

--- src/client.js
+++ src/client.js
@@ -1,14 +1,22 @@
 'use strict'
 
-const { get, STATUS_CODES } = require('http')
+const { get } = require('request')
 const url = require('url')
-const qs = require('querystring')
 const old = require('old')
-const concat = require('concat-stream')
-const regeneratorRuntime = require('regenerator-runtime')
-const watt = require('watt')
 const camel = require('camelcase')
 const tendermintMethods = require('./methods.js')
+
+function convertArgs (args) {
+  for (let k in args) {
+    let v = args[k]
+    if (Buffer.isBuffer(v)) {
+      args[k] = '0x' + v.toString('hex')
+    } else if (v instanceof Uint8Array) {
+      args[k] = '0x' + Buffer.from(v).toString('hex')
+    }
+  }
+  return args
+}
 
 class Client {
   constructor (uriString = 'localhost:46657') {
@@ -16,45 +24,36 @@ class Client {
     if (uri.protocol !== 'http' && uri.protocol !== 'tcp') {
       uri = url.parse(`http://${uriString}`)
     }
-    this.uri = uri
-    this.call = watt(this.call, { context: this })
+    this.uri = `http://${uri.hostname}:${uri.port}/`
   }
 
-  requestUrl (method, params) {
-    for (let k in params) {
-      let v = params[k]
-      if (Buffer.isBuffer(v)) {
-        params[k] = '0x' + v.toString('hex')
-      } else if (v instanceof Uint8Array) {
-        params[k] = '0x' + Buffer.from(v).toString('hex')
+  call (method, args, cb) {
+    get({
+      uri: method,
+      baseUrl: this.uri,
+      qs: convertArgs(args),
+      json: true
+    }, (err, res, data) => {
+      if (err) return cb(err)
+      if (res.statusCode !== 200) {
+        let err = `Server responded with status code ${res.statusCode}`
+        return cb(Error(err))
       }
-    }
-    let query = qs.stringify(params)
-    return `http://${this.uri.hostname}:${this.uri.port}/${method}?${query}`
-  }
-
-  * call (method, args, next) {
-    let res = yield get(this.requestUrl(method, args), next.arg(0))
-    if (res.statusCode !== 200) {
-      let err = `${res.statusCode} - ${STATUS_CODES[res.statusCode]}`
-      throw new Error(err)
-    }
-
-    let data = yield res.pipe(concat(next.arg(0))) // buffer response stream
-    data = JSON.parse(data.toString())
-    if (data.error) throw new Error(data.error)
-    return data.result
+      if (data.error) return cb(Error(data.error))
+      cb(null, data.result)
+    })
   }
 }
 
 // add methods to Client class based on methods defined in './methods.js'
 for (let name of tendermintMethods) {
   Client.prototype[camel(name)] = function (args, cb) {
+    if (!cb) {
+      cb = args
+      args = null
+    }
     return this.call(name, args, cb)
   }
 }
 
 module.exports = old(Client)
-
-// HACK: to prevent standard from complaining about not using `regeneratorRuntime`
-const noop = () => regeneratorRuntime; noop()
