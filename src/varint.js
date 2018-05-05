2e4424aa84c52dadb100f2a52f569d6036d215d5
Matt Bell <mappum@gmail.com>
2018-05-05 10:43:04 -0700

    Update for amino varint

--- src/varint.js
+++ src/varint.js
@@ -1,39 +1,27 @@
 'use strict'
 
-const MAX_VALUE = Math.pow(2, 53) - 1
-
-function decode(buffer, start = 0, end = buffer.length) {
-  let size = buffer[start]
-  let value = 0
-  for (let i = start + 1; i <= start + size; i++) {
-    value <<= 8
-    value |= buffer[i]
-  }
-  decode.bytes = size + 1
-  return value
+function decode (buffer, start = 0, end = buffer.length) {
+  throw Error('not implemented')
 }
 
-function encode(n, buffer = Buffer.alloc(encodingLength(n)), offset = 0) {
-  let size = encodingLength(n) - 1
-  buffer[offset] = size
-  for (let i = offset + size; i > offset; i--) {
-    buffer[i] = n & 0xff
-    n >>= 8
+function encode (n, buffer = Buffer.alloc(encodingLength(n)), offset = 0) {
+  let i = 0
+  while (n >= 0x80) {
+    buffer[offset + i] = (n & 0xff) | 0x80
+    n >>= 7
+    i++
   }
-  encode.bytes = size + 1
+  buffer[offset + i] = n
+  encode.bytes = i + 1
   return buffer
 }
 
-function encodingLength(n) {
-  if (n < 0 || n > MAX_VALUE) {
+function encodingLength (n) {
+  if (n < 0 || n > Number.MAX_SAFE_INTEGER) {
     throw new Error('varint value is out of bounds')
   }
-  let size = 1
-  while (n > 0) {
-    n >>= 8
-    size += 1
-  }
-  return size
+  let bits = Math.log2(n + 1)
+  return Math.ceil(bits / 7) || 1
 }
 
 module.exports = { encode, decode, encodingLength }
