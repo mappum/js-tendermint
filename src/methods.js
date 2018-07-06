65289041aaebdce53b064800450de064dedb15db
Matt Bell <mappum@gmail.com>
2018-07-06 11:58:53 -0700

    Updated RPC methods

--- src/methods.js
+++ src/methods.js
@@ -3,14 +3,20 @@
 module.exports = [
   'subscribe',
   'unsubscribe',
+  'unsubscribe_all',
 
   'status',
   'net_info',
+  'dial_peers',
   'dial_seeds',
   'blockchain',
   'genesis',
+  'health',
   'block',
+  'block_results',
+  'blockchain',
   'validators',
+  'consensus_state',
   'dump_consensus_state',
   'broadcast_tx_commit',
   'broadcast_tx_sync',
@@ -18,13 +24,13 @@ module.exports = [
   'unconfirmed_txs',
   'num_unconfirmed_txs',
   'commit',
+  'tx',
+  'tx_search',
 
   'abci_query',
   'abci_info',
-  'abci_proof',
 
   'unsafe_flush_mempool',
-  'unsafe_set_config',
   'unsafe_start_cpu_profiler',
   'unsafe_stop_cpu_profiler',
   'unsafe_write_heap_profile'
