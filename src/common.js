1052c79c438aae8bef62627352d11d95c820765c
Matt Bell <mappum@gmail.com>
2018-07-06 13:50:20 -0700

    Check number precision bounds in safeParseInt

--- src/common.js
+++ src/common.js
@@ -3,10 +3,13 @@
 function safeParseInt (nStr) {
   let n = parseInt(nStr)
   if (!Number.isInteger(n)) {
-    throw Error(`Value "${nStr}" is not an integer`)
+    throw Error(`Value ${JSON.stringify(nStr)} is not an integer`)
+  }
+  if (Math.abs(n) >= Number.MAX_SAFE_INTEGER) {
+    throw Error(`Absolute value must be < 2^53`)
   }
   if (String(n) !== String(nStr)) {
-    throw Error(`Value "${nStr}" is not a canonical integer string representation`)
+    throw Error(`Value ${JSON.stringify(nStr)} is not a canonical integer string representation`)
   }
   return n
 }
