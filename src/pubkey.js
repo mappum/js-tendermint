180654e7f692d6b9f335f9d0adaf28d95ab061d3
Matt Bell <mappum@gmail.com>
2019-02-19 17:14:06 -0800

    Truncate hash to 20 bytes for addresses

--- src/pubkey.js
+++ src/pubkey.js
@@ -4,7 +4,10 @@ const { tmhash } = require('./hash.js')
 
 function getAddress (pubkey) {
   let bytes = Buffer.from(pubkey.value, 'base64')
-  return tmhash(bytes).toString('hex').toUpperCase()
+  return tmhash(bytes)
+    .slice(0, 20)
+    .toString('hex')
+    .toUpperCase()
 }
 
 module.exports = { getAddress }
