f40f147860ae6e5e401e4bf803fc007eeea25563
Matt Bell <mappum@gmail.com>
2019-05-09 19:02:29 -0700

    Added a commit verify test fixture

--- test/verify.js
+++ test/verify.js
@@ -36,6 +36,96 @@ test('verifyCommit with mismatched header and precommit', (t) => {
   t.end()
 })
 
+test('verifyCommit with fixture', (t) => {
+  let validators = [
+    {
+      address: '8E593C2621AF81D46820DA7119805A1815510826',
+      pub_key: {
+        type: 'tendedrmint/PubKeyEd25519',
+        value: 'Q6Sw1toW1Vktd13mpwgJXMjOGk4TVQdsN9WfWN24/zs='
+      },
+      voting_power: 10,
+      proposer_priority: '0'
+    },
+    {
+      address: 'D39250AFFA7046E082C010E449952846F5C3E58C',
+      pub_key: {
+        type: 'tendermint/PubKeyEd25519',
+        value: '3zheJ3wd/jv1y5kCVvs6aG4oikgt5INv6rdz1m6gAE8='
+      },
+      voting_power: 10,
+      proposer_priority: '0'
+    }
+  ]
+  let header = { version: { block: '10', app: '0' },
+    chain_id: 'bitcoin-peg',
+    height: 22,
+    time: '2019-05-10T01:33:43.512766Z',
+    num_txs: '1',
+    total_txs: '13',
+    last_block_id:
+     { hash:
+        '9834F1E15D5EAFFD752844303CAC647E652C7D950A1EA50CD224295EAF9A2681',
+       parts:
+        { total: '1',
+          hash:
+           '357336D5D2E28298E80EEFB7C6E3A8E9B62CDD1A3F3ECF74CA2266242FDCFECF' } },
+    last_commit_hash:
+     '08EA1D192D3FCBB39FEAB21C4646BFA32658217424BBA07BF160EC130FA230DE',
+    data_hash:
+     'E97D243EDF5C278765CEB85E6C9FCDF21EC2863A5144168D933266B47BEF744F',
+    validators_hash:
+     'A06687B9F454F3ADCCAEF5CCCD474B2D60F11E261045935A54D249AF37B2EE67',
+    next_validators_hash:
+     'A06687B9F454F3ADCCAEF5CCCD474B2D60F11E261045935A54D249AF37B2EE67',
+    consensus_hash:
+     '048091BC7DDC283F77BFBF91D73C44DA58C3DF8A9CBC867405D8B7F3DAADA22F',
+    app_hash: '6894E993ED49F48157EDA6F138B419990A4DBA60',
+    last_results_hash:
+     '6E340B9CFFB37A989CA544E6BB780A2C78901D3FB33738768511A30617AFA01D',
+    evidence_hash: '',
+    proposer_address: 'D39250AFFA7046E082C010E449952846F5C3E58C' }
+  let commit = { block_id:
+   { hash:
+      '2A8EB50A5929E307BFEF94D19BDEBFF555D682ABFE5A1E6625511022BAFFB475',
+     parts:
+      { total: '1',
+        hash:
+         'B03B51C5BFCC6F7B9B55CFDAF9FAE4539A20E5250297E52317A64DF2752601CF' } },
+  precommits:
+   [ { type: 2,
+       height: '22',
+       round: '0',
+       block_id: { hash:
+          '2A8EB50A5929E307BFEF94D19BDEBFF555D682ABFE5A1E6625511022BAFFB475',
+         parts:
+          { total: '1',
+            hash:
+             'B03B51C5BFCC6F7B9B55CFDAF9FAE4539A20E5250297E52317A64DF2752601CF' } },
+       timestamp: '2019-05-10T01:33:45.283332Z',
+       validator_address: '8E593C2621AF81D46820DA7119805A1815510826',
+       validator_index: '0',
+       signature:
+        '1LKBIc+AdZgdhpgCT20C9kv8Gq+9zdyogOh6yAkYMy6TpY7R9qXN+SvMFK+tjXUmciSPYf707Xk1OaaKz/kqDg==' },
+     { type: 2,
+       height: '22',
+       round: '0',
+       block_id: { hash:
+          '2A8EB50A5929E307BFEF94D19BDEBFF555D682ABFE5A1E6625511022BAFFB475',
+         parts:
+          { total: '1',
+            hash:
+             'B03B51C5BFCC6F7B9B55CFDAF9FAE4539A20E5250297E52317A64DF2752601CF' } },
+       timestamp: '2019-05-10T01:33:45.494564Z',
+       validator_address: 'D39250AFFA7046E082C010E449952846F5C3E58C',
+       validator_index: '1',
+       signature:
+        '6EXjUmTCMOCYQNFusfO7hLuOtIy/jgBnK+gqiiFcRkCtS7P1oqIrS7G47PKu8FBoxTEyLD01P9KObNXO9Ha6Bg==' } ] }
+  verifyCommit(header, commit, validators)
+  t.pass()
+  t.end()
+})
+
 function genGenesisHeader (validators) {
   let validatorsHash = getValidatorSetHash(validators)
   return {
